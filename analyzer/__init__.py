6751531a7c853931dca69fb196642ff190215e79
liuwei9438 <595048689@qq.com>
2026-08-27 11:48:16 +0800

    Delete analyzer directory

--- analyzer/__init__.py
+++
@@ -1,4 +0,0 @@
-from .brand_relationship import classify_brand_relationship
-from .product_understanding import ProductUnderstandingEngine, UnderstandingError
-
-__all__ = ["ProductUnderstandingEngine", "UnderstandingError", "classify_brand_relationship"]
